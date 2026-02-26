70ba2eef4efda44b22b8cabbd1919069a3feb0ac
Lucas Viggiano <168561927+Lucasviggiano@users.noreply.github.com>
2026-02-26 08:47:22 -0300

    Update commands.js
    correção seletor

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -14,5 +14,5 @@ Cypress.Commands.add('completePurchase', () => {
         cy.get('[data-testid="addToCart"]').click()
         cy.get('[data-testid="selectAddressOrContinueToPayment"]').click()
         cy.get('[data-testid="completeCheckout"]').click()
-       cy.get(':nth-child(2) > :nth-child(1) > :nth-child(1) > :nth-child(1) > :nth-child(1) > [style="background-color: rgb(242, 242, 242); display: flex;"] > :nth-child(1) > :nth-child(1) > .r-13awgt0 > :nth-child(1) > .css-175oi2r > .css-146c3p1').should('have.text', 'Order Success')
+        cy.contains('Order Success').should('be.visible')
 })
